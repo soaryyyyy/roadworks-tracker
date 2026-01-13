eeb6b00c558cf5b60c45e291aa341b0f357f9e72
soaryyyyy <rabezandrinasoary@gmail.com>
2026-01-13 14:53:42 +0300

    structure backend
    initialisation spring boot

---
+++ backend/src/main/java/itu/p17/s5/cloud/roadworks_tracker/RoadworksTrackerApplication.java
@@ -0,0 +1,13 @@
+package itu.p17.s5.cloud.roadworks_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadworksTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadworksTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/itu/p17/s5/cloud/roadworks_tracker/RoadworksTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package itu.p17.s5.cloud.roadworks_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadworksTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
